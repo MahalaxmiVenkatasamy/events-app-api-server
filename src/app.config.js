d6d78476e00a97220316cd4b564aa4bba0c7a44b
MahalaxmiVenkatasamy <mahalaxmi.venkatasamy@gmail.com>
2022-10-06 16:56:55 +0000

    Name Changed

--- src/app.config.js
+++ src/app.config.js
@@ -4,7 +4,7 @@
  * DO NOT store state here - must be part of a stateless service
  */
 class AppConfig {
-    team = 'Your Team Name';
+    team = 'Rockers now Royals';
     version = '1.0.0';
 }
 module.exports = AppConfig;
